9d6db6710f1551f45f0b512fbd3a42c5945c074b
Joalch4 <106455570+Joalch4@users.noreply.github.com>
2024-05-21 23:22:24 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import pygame
 import sys
 import os
@@ -6,8 +7,8 @@ from tkinter import Tk, filedialog
 
 class Pixaint:
     def __init__(self):
+        # Inicialización de Pygame y definición de constantes
         pygame.init()
-        # Configuración inicial de la interfaz y la cuadrícula
         self.CELL_SIZE = 20
         self.ROWS = 20
         self.COLS = 20
@@ -20,44 +21,41 @@ class Pixaint:
         self.HEIGHT = self.GRID_HEIGHT + 2 * (self.BUTTON_SIZE + 10)
         self.GRID_X_OFFSET = (self.WIDTH - self.GRID_WIDTH) // 2
         self.GRID_Y_OFFSET = (self.HEIGHT - self.GRID_HEIGHT) // 2
-
-        # Inicialización de la ventana del juego
+        
+        # Configuración de la pantalla y título de la ventana
         self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
         pygame.display.set_caption('Pixaint')
 
-        # Definición de colores
+        # Definición de colores y símbolos de los botones
         self.WHITE = (255, 255, 255)
         self.BLACK = (0, 0, 0)
         self.BORDER_COLOR = self.BLACK
-        # Colores para los botones
         self.BUTTON_COLORS = [
-            (255, 0, 0),    # Rojo
-            (152, 251, 152),# Verde claro
-            (0, 0, 255),    # Azul
-            (255, 255, 0),  # Amarillo
-            (255, 192, 203),# Rosado
-            (0, 255, 255),  # Celeste
-            (128, 128, 128),# Gris
-            (255, 165, 0),  # Naranja
-            (128, 0, 128),  # Morado
-            (0, 128, 0),    # Verde oscuro
-            (255, 0, 255),  # Blanco
+            (255, 0, 0),    # Rojo (Botón 18)
+            (152, 251, 152),# Verde claro (Botón 19)
+            (0, 0, 255),    # Azul (Botón 20)
+            (255, 255, 0),  # Amarillo (Botón 21)
+            (255, 192, 203),# Rosado (Botón 22)
+            (0, 255, 255),  # Celeste (Botón 23)
+            (128, 128, 128),# Gris (Botón 24)
+            (255, 165, 0),  # Naranja (Botón 25)
+            (128, 0, 128),  # Morado (Botón 26)
+            (0, 128, 0),    # Verde oscuro (Botón 27)
+            (255, 0, 255),  # Blanco (Botón 28)
         ]
-        # Símbolos para los botones
         self.BUTTON_SYMBOLS = [
             ".", ":", "-", "=", "¡", "&", "$", "%", "@", "+", "#"
         ]
-        # Carga de imágenes para los botones
         self.button_images = []
         for i in range(1, 18):
             image_path = f"Pixaint-main/imagenes_pixaint/boton_{i}.png"
             self.button_images.append(pygame.image.load(image_path))
 
-        # Inicialización de la cuadrícula
+        # Creación de la cuadrícula y lista de botones
         self.grid = [[self.WHITE] * self.COLS for _ in range(self.ROWS)]
         self.original_grid = [[self.WHITE] * self.COLS for _ in range(self.ROWS)]
         self.buttons = []
-        # Configuración de la posición y texto de los botones
+        # Creamos los rectángulos para los botones y su texto asociado
         for i in range(self.NUM_BUTTONS // 4):
             self.buttons.append({"rect": pygame.Rect(self.GRID_X_OFFSET - self.BUTTON_SIZE - 10, self.GRID_Y_OFFSET + i * (self.BUTTON_SIZE + self.BUTTON_PADDING), self.BUTTON_SIZE, self.BUTTON_SIZE), "text": f"Botón {i+1}"})
         for i in range(self.NUM_BUTTONS // 4):
@@ -66,10 +64,7 @@ class Pixaint:
             self.buttons.append({"rect": pygame.Rect(self.GRID_X_OFFSET + i * (self.BUTTON_SIZE + self.BUTTON_PADDING), self.GRID_Y_OFFSET - self.BUTTON_SIZE - 10, self.BUTTON_SIZE, self.BUTTON_SIZE), "text": f"Botón {i+1+self.NUM_BUTTONS//2}"})
         for i in range(self.NUM_BUTTONS // 4):
             self.buttons.append({"rect": pygame.Rect(self.GRID_X_OFFSET + i * (self.BUTTON_SIZE + self.BUTTON_PADDING), self.GRID_Y_OFFSET + self.GRID_HEIGHT + 10, self.BUTTON_SIZE, self.BUTTON_SIZE), "text": f"Botón {i+1+3*(self.NUM_BUTTONS//4)}"})
-        
-        # Color seleccionado inicialmente
         self.selected_color = self.WHITE
-        # Estado de visualización de números y símbolos
         self.showing_numbers = False
         self.showing_symbols = False
 
@@ -89,7 +84,7 @@ class Pixaint:
         sys.exit()
 
     def handle_mouse_click(self, mouse_pos):
-        # Manejo de clics del mouse en botones y cuadrícula
+        # Manejo de clics del mouse
         button_clicked = False
         for idx, button in enumerate(self.buttons[17:]):
             if button["rect"].collidepoint(mouse_pos):
@@ -119,6 +114,14 @@ class Pixaint:
                         self.clear_grid()
                         button_clicked = True
                         break
+                    elif idx == 8:
+                        self.rotate_right()
+                        button_clicked = True
+                        break
+                    elif idx == 9:
+                        self.rotate_left()
+                        button_clicked = True
+                        break
             if not button_clicked:
                 grid_x = (mouse_pos[0] - self.GRID_X_OFFSET) // self.CELL_SIZE
                 grid_y = (mouse_pos[1] - self.GRID_Y_OFFSET) // self.CELL_SIZE
@@ -126,9 +129,10 @@ class Pixaint:
                     self.grid[grid_y][grid_x] = self.selected_color
 
     def save_grid(self):
-        # Guardar la cuadrícula actual en un archivo de texto
+        # Función para guardar la cuadrícula en un archivo de texto
         if not os.path.exists('Pixaint-main/imagenes_guardadas'):
             os.makedirs('Pixaint-main/imagenes_guardadas')
+        # Generar el nombre del archivo basado en la fecha y hora actuales
         timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
         filename = os.path.join('Pixaint-main/imagenes_guardadas', f'pixaint_grid_{timestamp}.txt')
         with open(filename, 'w') as file:
@@ -137,8 +141,8 @@ class Pixaint:
         print(f"Cuadrícula guardada en {filename}")
 
     def load_grid(self):
-        # Cargar una cuadrícula desde un archivo de texto seleccionado por el usuario
-        Tk().withdraw()
+    # Función para cargar la cuadrícula desde un archivo de texto usando un diálogo de selección de archivos
+        Tk().withdraw()  # Oculta la ventana principal de Tkinter
         filepath = filedialog.askopenfilename(
             initialdir="Pixaint-main/imagenes_guardadas",
             title="Seleccione el archivo de cuadrícula",
@@ -152,40 +156,40 @@ class Pixaint:
                 numbers = line.strip().split(' ')
                 for col_idx, number in enumerate(numbers):
                     if row_idx < self.ROWS and col_idx < self.COLS:
-                        self.grid[row_idx][col_idx] = self.number_to_color(int(number))
+                        self.grid[row_idx][col_idx] = self.number_to_color(int(number))  # Convertir número a color
         print(f"Cuadrícula cargada desde {filepath}")
 
     def draw(self):
-        # Dibujar la interfaz del juego
+        # Función para dibujar la pantalla
         self.screen.fill(self.WHITE)
         for idx, button in enumerate(self.buttons):
+            # Se dibujan las imágenes de los botones si están en el rango de botones con imágenes, de lo contrario, se dibujan rectángulos de colores
             if idx < 17:
-                # Dibujar botones con imágenes
                 self.screen.blit(self.button_images[idx], button["rect"].topleft)
             else:
-                # Dibujar botones con colores
                 pygame.draw.rect(self.screen, self.BUTTON_COLORS[idx - 17], button["rect"], border_radius=5)
         for row in range(self.ROWS):
             for col in range(self.COLS):
                 rect = pygame.Rect(self.GRID_X_OFFSET + col * self.CELL_SIZE, self.GRID_Y_OFFSET + row * self.CELL_SIZE, self.CELL_SIZE, self.CELL_SIZE)
                 color = self.grid[row][col]
                 if self.showing_numbers and isinstance(color, int):
-                    # Mostrar números en la cuadrícula
+                    # Si estamos mostrando números, se muestra el número en lugar del color
                     text_surface = pygame.font.SysFont(None, 24).render(str(color), True, self.BLACK)
                     self.screen.blit(text_surface, rect.topleft)
                 elif self.showing_symbols and isinstance(color, str):
-                    # Mostrar símbolos en la cuadrícula
+                    # Si estamos mostrando símbolos, se muestra el símbolo en lugar del color
                     text_surface = pygame.font.SysFont(None, 24).render(color, True, self.BLACK)
                     self.screen.blit(text_surface, rect.topleft)
                 else:
-                    # Mostrar colores en la cuadrícula
+                    # Si estamos mostrando colores, se dibuja el rectángulo del color
                     pygame.draw.rect(self.screen, color if not isinstance(color, int) else self.WHITE, rect)
+                    # Se dibuja el borde del rectángulo
                     pygame.draw.rect(self.screen, self.BORDER_COLOR, rect, 1)
 
         pygame.display.flip()
 
     def color_to_number(self, color):
-        # Convertir un color en su número correspondiente
+        # Función para convertir un color en su número correspondiente
         if color == self.WHITE:
             return 0
         for idx, col in enumerate(self.BUTTON_COLORS):
@@ -194,9 +198,8 @@ class Pixaint:
         return -1
 
     def toggle_grid(self):
-        # Alternar entre mostrar colores y números en la cuadrícula
+        # Alterna entre colores, numeros o simbolos
         if self.showing_symbols:
-            # Convertir símbolos a números si se estaban mostrando
             for row in range(self.ROWS):
                 for col in range(self.COLS):
                     self.grid[row][col] = self.symbol_to_number(self.grid[row][col])
@@ -205,17 +208,15 @@ class Pixaint:
         else:
             self.showing_numbers = not self.showing_numbers
             if self.showing_numbers:
-                # Almacenar la cuadrícula original antes de convertir los colores en números
                 self.original_grid = [row[:] for row in self.grid]
                 for row in range(self.ROWS):
                     for col in range(self.COLS):
                         self.grid[row][col] = self.color_to_number(self.grid[row][col])
             else:
-                # Restaurar la cuadrícula original cuando se muestra de nuevo en colores
                 self.grid = [row[:] for row in self.original_grid]
 
     def color_to_symbol(self, color):
-        # Convertir un color en su símbolo correspondiente
+        # Convertir colores a símbolos
         if color == self.WHITE:
             return ""
         for idx, col in enumerate(self.BUTTON_COLORS):
@@ -224,9 +225,8 @@ class Pixaint:
         return "?"
 
     def toggle_symbols(self):
-        # Alternar entre mostrar colores y símbolos en la cuadrícula
+        # Alterna entre colores, numeros o simbolos
         if self.showing_numbers:
-            # Convertir números a símbolos si se estaban mostrando
             for row in range(self.ROWS):
                 for col in range(self.COLS):
                     self.grid[row][col] = self.number_to_symbol(self.grid[row][col])
@@ -235,18 +235,16 @@ class Pixaint:
         else:
             self.showing_symbols = not self.showing_symbols
             if self.showing_symbols:
-                # Convertir colores a símbolos si se está mostrando
                 for row in range(self.ROWS):
                     for col in range(self.COLS):
                         self.grid[row][col] = self.color_to_symbol(self.grid[row][col])
             else:
-                # Restaurar colores desde símbolos cuando se muestran de nuevo
                 for row in range(self.ROWS):
                     for col in range(self.COLS):
                         self.grid[row][col] = self.symbol_to_color(self.grid[row][col])
 
     def clear_grid(self):
-        # Limpiar la cuadrícula estableciendo todos los valores en blanco
+        # Limpia la cuadrícula
         self.showing_numbers = False
         self.showing_symbols = False
         for row in range(self.ROWS):
@@ -254,7 +252,7 @@ class Pixaint:
                 self.grid[row][col] = self.WHITE
 
     def symbol_to_color(self, symbol):
-        # Convertir un símbolo en su color correspondiente
+        # Función para convertir un símbolo en su color correspondiente
         if symbol == "":
             return self.WHITE
         for idx, sym in enumerate(self.BUTTON_SYMBOLS):
@@ -263,15 +261,28 @@ class Pixaint:
         return self.WHITE
 
     def number_to_color(self, number):
-        # Convertir un número en su color correspondiente
+        # Función para convertir un número en su color correspondiente
         if number == 0:
             return self.WHITE
         elif 1 <= number <= len(self.BUTTON_COLORS):
             return self.BUTTON_COLORS[number - 1]
         return self.WHITE
-
+    
+    def rotate_right(self):
+        # Rota la matriz hacia la derecha
+        transposed_grid = [[self.grid[col][row] for col in range(self.ROWS)] for row in range(self.COLS)]
+        for row in range(self.ROWS):
+            transposed_grid[row] = transposed_grid[row][::-1]
+        self.grid = transposed_grid
+    
+    def rotate_left(self):
+        # Rota la matriz a la izquierda
+        for col in range(self.COLS):
+            self.grid[col] = self.grid[col][::-1]
+        self.grid = [[self.grid[row][col] for row in range(self.COLS)] for col in range(self.ROWS)]
+    
     def symbol_to_number(self, symbol):
-        # Convertir un símbolo en su número correspondiente
+        # Alterna entre simbolo y numero
         if symbol == "":
             return 0
         for idx, sym in enumerate(self.BUTTON_SYMBOLS):
@@ -280,7 +291,7 @@ class Pixaint:
         return -1
 
     def number_to_symbol(self, number):
-        # Convertir un número en su símbolo correspondiente
+        # Alterna entre numero y simbolo
         if number == 0:
             return ""
         elif 1 <= number <= len(self.BUTTON_SYMBOLS):
@@ -288,6 +299,5 @@ class Pixaint:
         return "?"
 
 if __name__ == "__main__":
-    # Inicializar y ejecutar el juego Pixaint
     game = Pixaint()
     game.run()
